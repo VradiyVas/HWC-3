39d44b79c31ec9e031ff1ed56fb56ffa0deeb36e
Vasily Vradiy <promo.spb.vradiy.v@gmail.com>
2023-03-09 14:10:27 +0300

    task1 completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"xисло: {number} - палиндром.");
+    }
+    else Console.WriteLine($" число: {number} - Не палиндром.");
+}
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
